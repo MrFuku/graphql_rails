ccfc296d0a5420f9e4d5916d30ba86a2a9ec70c8
MrFuku <mmafighter_shun@icloud.com>
2022-04-02 00:41:19 +0900

    Labelも含めたクエリ

--- app/graphql/types/label_type.rb
+++ app/graphql/types/label_type.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 module Types
-  class PostType < Types::BaseObject
+  class LabelType < Types::BaseObject
     field :id, ID, null: false
-    field :user, UserType, null: false
-    field :label, LabelType, null: true
-    field :body, String
+    field :post, PostType, null: true
+    field :name, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
